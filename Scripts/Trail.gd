d1090229976ab23cefaa10e9b24a4ce6613c7256
FanuelAnte <apexfanuel@gmail.com>
2024-01-15 11:46:41 +0300

    Wing Trails

--- Scripts/Trail.gd
+++ Scripts/Trail.gd
@@ -1,12 +1,13 @@
 extends Line2D
 
+export (int) var max_trail_length = 5
 var point
 
 func _ready():
 	set_as_toplevel(true)
-
+	
 func _process(delta):
 	point = get_parent().global_position
 	add_point(point)
-	if points.size() > 10:
+	if points.size() > max_trail_length:
 		remove_point(0)
