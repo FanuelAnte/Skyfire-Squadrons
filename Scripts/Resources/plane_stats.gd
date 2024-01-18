05dc93282dcbf64d7eccdea5f553e426d2b8dd68
FanuelAnte <apexfanuel@gmail.com>
2024-01-18 17:20:03 +0300

    UI Improvements.

--- Scripts/Resources/plane_stats.gd
+++ Scripts/Resources/plane_stats.gd
@@ -2,8 +2,8 @@ extends Resource
 
 class_name plane_resource
 
-export (int) var wingspan = 50
-export (int) var speed = 400
+export (int) var wingspan = 40
+export (int) var speed = 300
 export (int) var bank_angle = 10
 export (float) var max_bank_angle_factor = 1.5
 export (float) var max_health = 100
